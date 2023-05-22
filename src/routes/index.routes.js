1561804ac80ae05191d611b312377424967f4719
Mashita Dewi <mashitaad@gmail.com>
2023-05-22 22:03:32 +0700

    fix: fix initial commit

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/home/HomePage";
 
 function IndexRoutes() {
     return(
         <Router>
             <Routes>
-
+                <Route path="/" element={<HomePage />} />
             </Routes>
         </Router>
     );
