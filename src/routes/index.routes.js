010ba3dfbf83cd9527964821673680610058123b
Mashita Dewi <mashitaad@gmail.com>
2023-05-22 21:48:40 +0700

    feat: routes

---
+++ src/routes/index.routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function IndexRoutes() {
+    return(
+        <Router>
+            <Routes>
+
+            </Routes>
+        </Router>
+    );
+}
+
+export default IndexRoutes;
