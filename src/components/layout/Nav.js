7b820324546de03442b66361494457fc55f2a4c2
Mashita Dewi <mashitaad@gmail.com>
2023-05-24 15:03:20 +0700

    feat: Add uncomplete Navbar

---
+++ src/components/layout/Nav.js
@@ -0,0 +1,67 @@
+import React from "react";
+import 'bootstrap/js/dist/dropdown';
+import { FaUserCircle } from 'react-icons/fa';
+
+function Nav({Toggle}) {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-white bg-white px-3">
+      <i className="navbar-brand bi bi-justify-left fs-5" onClick={Toggle}></i>
+      <button
+        className="navbar-toggler d-lg-none"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#collapsibleNavId"
+        aria-controls="collapsibleNavId"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      ></button>
+      <div className="collapse navbar-collapse" id="collapsibleNavId">
+        <form className="d-flex ms-auto my-lg-0">
+          <div className="input-group">
+            <input
+              className="form-control rounded-0"
+              type="text"
+              placeholder=""
+              style={{
+                backgroundImage: `url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="%23333" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><circle cx="11" cy="11" r="8"/><path d="M21 21l-4.35-4.35"/></svg>')`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "8px 8px",
+                paddingLeft: "35px"
+              }}
+            />
+          </div>
+          <button
+            className="btn btn-outline-primary rounded-0 my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+        <ul className="navbar-nav mt-2 mt-lg-0">
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="dropdownId"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Unis Badri
+            </a>
+            <div className="dropdown-menu" aria-labelledby="dropdownId">
+              <a className="dropdown-item" href="#">
+                Action 1
+              </a>
+              <a className="dropdown-item" href="#">
+                Action 2
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
