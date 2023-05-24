27cb72397f5fbc6ab428ec0e3b7981dcc55ebd81
Mashita Dewi <mashitaad@gmail.com>
2023-05-24 15:03:50 +0700

    feat: Add uncomplete sidebar

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './styles/sidebar.css'
+import logo from '../../assets/icons/logo.png'
+
+function Sidebar(){
+    return(
+        <div className="bg-white sidebar p-2">
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <hr className="text-dark"/>
+            <div className="list-group list-group-flush">
+                <a className="list-group-item py-2">
+                    <span>Dashboard</span>
+                </a>
+                <a className="list-group-item py-2">
+                    <span>Cars</span>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
