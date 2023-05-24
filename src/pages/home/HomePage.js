ef13d38bbd1249beb65f1a52bec8a4c6e084f93c
Mashita Dewi <mashitaad@gmail.com>
2023-05-24 15:09:01 +0700

    refactor: modified

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Sidebar from "../../components/layout/Sidebar";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Header from "../../components/layout/Header";
+import Mainmenu from "../../components/layout/Mainmenu";
 
 export default function HomePage() {
-    return(
-        <>
-        <h1>Homepage</h1>
-        </>
-    )
+
+    const [toggle, setToggle] = useState(true);
+    const Toggle = () => {
+        setToggle(!toggle);
+    }
+
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                {/* <div className="col">
+                    <Mainmenu />
+                </div> */}
+                {toggle && <div className="col-2 bg-white vh-100 position-fixed p-0">
+                    <Sidebar />
+                </div>}
+                <div className={`col px-0 ${toggle ? 'offset-2' : ''}`}>
+                    <Header Toggle={Toggle} />
+                </div>
+            </div>
+        </div>
+    );
 }
