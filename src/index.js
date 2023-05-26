85f2cc842db8a4648a4761fb48cd801a44ebd630
irvantaufik28 <irvantaufik28@gmail.com>
2023-05-26 23:38:27 +0700

    setup redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 // import { Provider } from 'react-redux';
 import IndexRoutes from './routes/index.routes';
-
+import { Provider } from 'react-redux';
+import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <IndexRoutes />
-  </React.StrictMode>
+  <Provider store={store}>
+   <IndexRoutes />
+  </Provider>
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
