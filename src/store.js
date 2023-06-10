b35cdfa7f5a23b6d203dcdbae877d66e94051a25
irvantaufik28 <irvantaufik28@gmail.com>
2023-06-10 23:37:07 +0700

    order Slicer Reducer redux

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/authSlice'
+import orderReducer from './features/orderSlice'
 
 const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    order: orderReducer
   },
 });
 
