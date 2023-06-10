74ca6ba7080b904399caf14b9fa5d92941aa0cb2
irvantaufik28 <irvantaufik28@gmail.com>
2023-06-11 00:00:49 +0700

    car reducer redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/authSlice'
 import orderReducer from './features/orderSlice'
+import carReducer from './features/carSlice'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    car: carReducer,
     order: orderReducer
   },
 });
