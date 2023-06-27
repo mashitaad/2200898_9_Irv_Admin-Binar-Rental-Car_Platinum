b9b83d7799c856525b20be0e92d184eef74af604
unknown <sadynursholihin@gmail.com>
2023-06-27 20:49:25 +0700

    acction delete modal dialog

--- src/pages/car/components/CarCard.js
+++ src/pages/car/components/CarCard.js
@@ -4,10 +4,12 @@ import { BsPeople } from "react-icons/bs";
 import { IoMdTime } from "react-icons/io";
 import { format } from "date-fns";
 import DialogBox from "./DialogBox";
-import '../styles/carCard.css'
+import "../styles/carCard.css";
 import ImageWithLoading from "../../../components/ui/ImageWithLoading";
-import nullImage from "../../../assets/images/imagenotfound.jpeg"
-import { Link } from "react-router-dom";
+import nullImage from "../../../assets/images/imagenotfound.jpeg";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import config from "../../../config";
+import axios from "axios";
 
 const CarCard = ({ car }) => {
   const getCategoryText = (category) => {
@@ -20,60 +22,66 @@ const CarCard = ({ car }) => {
     return categoryMap[category] || "";
   };
 
+  const navigate = useNavigate();
   const formatDate = (date) => {
     return format(new Date(date), "d MMMM yyyy, HH:mm");
   };
+  const token = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("token="))
+    ?.split("=")[1];
+  const url = config.apiBaseUrl;
+  const handleDelete = async () => {
+    // https://bootcamp-rent-cars.herokuapp.com/admin/car/2650
 
-  const handleDelete = () => {
-    // Logika penghapusan data atau tindakan lainnya
-    console.log("Delete car:", car);
+    try {
+      const response = await axios.delete(url + `/admin/car/${car.id}`, {
+        headers: {
+          access_token: token,
+        },
+      });
+      navigate("/admin/car/list");
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
-          <Col md='4'>
-            <Card>
-              {car.image === null ?
-                (
-                  <div>
-                    <ImageWithLoading
-                      src={nullImage}
-                      alt={'null'}
-                    />
-                  </div>
-                ) :
-                (
-                  <div>
-                    <ImageWithLoading
-                      src={car.image}
-                      alt={car.name}
-                    />
-                  </div>
-                )
-              }
-              <Card.Body>
-                <Card.Title>{car.name}</Card.Title>
-                <Card.Subtitle className="mb-2">Rp {car.price} / hari</Card.Subtitle>
-                <Card.Text>
-                  <BsPeople className="mb-1 me-1" /> {getCategoryText(car.category)}
-                </Card.Text>
-                <Card.Text>
-                  <IoMdTime /> Updated at {formatDate(car.updatedAt)}
-                </Card.Text>
-                <div className="row justify-content-between button-card">
-                  <div className="col-md-6">
-                    <DialogBox onDelete={handleDelete} />
-                  </div>
-                  <div className="col-md-6">
-                    <Link to={`/admin/update/car/${car.id}`}>
-                    <Button className="w-100" variant="outline-success">
-                      Edit
-                    </Button>
-                    </Link>
-                  </div>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-     
+    <Col md="4">
+      <Card>
+        {car.image === null ? (
+          <div>
+            <ImageWithLoading src={nullImage} alt={"null"} />
+          </div>
+        ) : (
+          <div>
+            <ImageWithLoading src={car.image} alt={car.name} />
+          </div>
+        )}
+        <Card.Body>
+          <Card.Title>{car.name}</Card.Title>
+          <Card.Subtitle className="mb-2">Rp {car.price} / hari</Card.Subtitle>
+          <Card.Text>
+            <BsPeople className="mb-1 me-1" /> {getCategoryText(car.category)}
+          </Card.Text>
+          <Card.Text>
+            <IoMdTime /> Updated at {formatDate(car.updatedAt)}
+          </Card.Text>
+          <div className="row justify-content-between button-card">
+            <div className="col-md-6">
+              <DialogBox onDelete={handleDelete} />
+            </div>
+            <div className="col-md-6">
+              <Link to={`/admin/update/car/${car.id}`}>
+                <Button className="w-100" variant="outline-success">
+                  Edit
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 };
 
