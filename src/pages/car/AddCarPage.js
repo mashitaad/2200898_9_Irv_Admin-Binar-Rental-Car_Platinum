0a6998619a786963dff4ca7d156d6f650f3f1c6f
irvantaufik28 <irvantaufik28@gmail.com>
2023-07-03 14:25:57 +0700

    delete coverage

--- src/pages/car/AddCarPage.js
+++ src/pages/car/AddCarPage.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import AddCard from './components/AddCar'
 import SideBar from '../../components/layout/SideBar'
-import { useDispatch } from 'react-redux'
-import { adminAddCar } from '../../features/carSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { adminAddCar, carSelectors } from '../../features/carSlice'
 import { useNavigate } from 'react-router-dom'
 
 export default function AddCarPage() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const loading = useSelector(carSelectors.loading)
 const handleAddCar = async (payload) => {
 
   try {
     await dispatch(adminAddCar(payload)).unwrap()
-    navigate('/admin/car/list')
+    if(loading) {
+      navigate('/admin/car/list')
+
+    }
     
   } catch (error) {
     console.log(error)
